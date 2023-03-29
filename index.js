5bdb11a01a81b530a9998f762f0a9dd377fa7c0a
DuMark Allman <dumark.allman@yahoo.com>
2023-03-28 20:20:15 -0400

    [StewieDuey]

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+const { arrayContaining } = require("expect");
+
+function findMatching(driverArray, name) {
+  return driverArray.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  };
+
+  function fuzzyMatch(driverArray, name) {
+    return driverArray.filter(person=> person[0].toLowerCase() === name[0].toLowerCase() && person[1].toLowerCase() === name[1].toLowerCase());
+    };
+
+  function matchName(driverArray, name) {
+    return driverArray.filter(person=> person.name === name);
+  }
